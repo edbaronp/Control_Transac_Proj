869f3bea7649be88dc5d2530f2ec114d6ef5a46f
edbaronp <edbaronp@unal.edu.co>
2018-02-12 11:58:20 -0500

    Todos los generadores funcionan al pelo :) con la función promedio corregida
    Falta agregar cargas y probarlas

--- Baron2017_projected_constraints_gen_Loads_15nodes.m
+++ Baron2017_projected_constraints_gen_Loads_15nodes.m
@@ -6,11 +6,8 @@ clear all
 
 %%%% Inputs %%%%
 %Graph Declaration%
-nodes=15;
-Edges=[5 15;5 3;5 4;2 3; 2 6; 3 1; 4 1; 4 15;
-    4 10;1 6;1 9;1 8;6 7;1 13;7 13;8 11;
-    8 12;9 10;9 11;10 14;10 11;11 12;12 13;
-    14 15];
+nodes=4;
+Edges=[ 1 2; 1 3; 2 4; 3 4];
 power=ones(nodes,1);
 %%%%% System global values%%%%%%%
 Function_prom=zeros(1,nodes);
@@ -19,32 +16,36 @@ Power_demanded=zeros(nodes,1);
 Power_generated=zeros(nodes,1);
 %%%%% Número de nodos
 Number_generators=zeros(nodes,1);%Numero de generadores
-Choose_gen=[1;1;1;1;1;0;0;0;0;0;0;0;0;0;0];%1=gen o 0=consumidores
+Choose_gen=[1;1;1;1];%1=gen o 0=consumidores
 %%%%%Separación vectores
 xg=zeros(nodes,1);
 power_aux=zeros(nodes,1);
 %%%% Parametros simulación
 iterations=90000;
-alpha=0.7;
+alpha=0.5;
 %Generator Charateristics
-generation_cost=[0.2;0.25;0.27;0.29;0.23;0;0;0;0;0;0;0;0;0;0] ;
+generation_cost=[1;2;3;4] ;
 %Generation Constraints
-Pmin=[1;1;1;1;1;1;1;1;1;1;1;1;1;1;1];
-Pmax=[5000;5000;5000;5000;5000;1000;1500;1;1;1;1;1;1;1;1];
+Pmin=[1;1;1;1];
+Pmax=[2000;3000;4000;5000];
 %Rate of change
-Rate_change=[1;1;1;1;1;1;1;1;1;1;1;1;1;1;1];
+Rate_change=[0.3;0.3;0.3;0.3];
 %Load
-Load_1=[0;0;0;0;0;1200;1200;1200;1200;1200;1200;1200;1200;1200;1200];
+Load_1=[0;0;0;8000];
 Load=Load_1;
 asd=0;
+%Projection auxiliar variable
+full_generators=zeros(nodes,1);
 
 for k=1:iterations
+    
+ projection_generation=zeros(nodes,1);
  %%%%%%%%%%%%Demanded Power%%%%%    
      if (k==30000)
-         Load=[0;0;0;0;0;1500;1500;1500;1500;1500;1500;1500;1500;1500;1500];
+         Load=[0;0;0;14000];
      end    
     if (k==60000)
-       Load=[0;0;0;0;0;2000;2000;2000;2000;2000;2000;2000;2000;2000;2000];
+       Load=[0;0;0;10000];
      end 
 %%%%%%%%%%%% Spanning Tree %%%%%%%%%%
 if(asd==0)
@@ -75,7 +76,7 @@ for j=1:nodes
 t=0;
 for i=1:nodes
     if(Adjacency_Graph(i,j)==1)
-       Function_prom(1,j)=Function_prom(1,j)+power(i);
+       Function_prom(1,j)=Function_prom(1,j)+power(i,k);
        t=t+1;
     end
 end
@@ -86,21 +87,14 @@ end
 Gradient=[(1/generation_cost(1))*(1-power(1,k)/Pmax(1));
           1/generation_cost(2)*(1-power(2,k)/Pmax(2)); 
           1/generation_cost(3)*(1-power(3,k)/Pmax(3));
-          1/generation_cost(4)*(1-power(4,k)/Pmax(4));
-          1/generation_cost(5)*(1-power(5,k)/Pmax(5));
-          1/Global_cost(6,k)*(1-(power(6,k))/Pmax(6));
-          1/Global_cost(7,k)*(-1+(power(7,k))/Pmax(7));
-          0;0;0;0;0;0;0;0];
-
-Anita(k)=1/Global_cost(7,k)*(-1+(power(7,k))/Pmax(7));
-Eder(k) =1/Global_cost(6,k)*(1-(power(6,k))/Pmax(6));
+          1/generation_cost(4)*(1-power(4,k)/Pmax(4))];
 
 
 for i=1:nodes
 if(Choose_gen(i)~=1)
-    power(i,k+1)=power(i,k)+(Function_prom(i)-alpha*Gradient(i));
+    power(i,k+1)=-power(i,k)+(Function_prom(i)-alpha*Gradient(i));
 else
-    power(i,k+1)=power(i,k)-(Function_prom(i)-alpha*Gradient(i));
+    power(i,k+1)=-power(i,k)+(Function_prom(i)-alpha*Gradient(i));
 end
 end
 
@@ -114,8 +108,49 @@ end
 end
 
 %%%%%Projected Constrains%%%%%
-%%%%%Virtual Agent Global Constraint%%%%%%%%
+%%Global Constraint%%
 power(:,k+1)= power(:,k+1)-(Power_generated(:,k)-Power_demanded(:,k))./Number_generators(:,k);
+
+%%%%% Local Rate of change constraint%%%%
+for e=1:nodes
+  if(power(e,k+1)-power(e,k)>Rate_change(e))
+       power(e,k+1)=power(e,k)+Rate_change(e);
+  end
+  if(power(e,k+1)-power(e,k)<-Rate_change(e))
+       power(e,k+1)=power(e,k)-Rate_change(e);
+  end
+end
+%%%%% Local Generation constraints%%%%
+for q=1:nodes
+  if(power(q,k+1)<Pmin(q))
+       power(q,k+1)=Pmin(q);
+  end
+end
+
+for w=1:nodes
+  if(power(w,k+1)>Pmax(w))
+       power(w,k+1)=Pmax(w);
+  end
+end
+
+Anita(1,k)= power(2,k+1);
+
+%%Global Constraint with generators with maximum capacity%%
+for c=1:nodes
+    if (isequal(power(c,k+1),Pmax(c)))
+        projection_generation(c)=projection_generation(c)+1;
+    end
+end
+
+if(~isequal(projection_generation(:),zeros(1,nodes)))
+    full_generators(:,k)=DistCons(projection_generation,Adjacency_MST);
+        for j=1:nodes
+            if (power(j,k+1)~=Pmax(j))
+                power(j,k+1)= power(j,k+1)-(Power_generated(j,k)-Power_demanded(j,k))./(Number_generators(j,k)-full_generators(j,k));
+            end
+        end
+end
+
 
 
 %%%%% Local Rate of change constraint%%%%
@@ -140,30 +175,24 @@ for w=1:nodes
   end
 end
 
-v(:,k)=((ones(1,nodes)*power(:,k+1))*ones(nodes,1)-Power_demanded(:,k)-(ones(1,nodes)*power_aux(:,k+1))*ones(nodes,1))./Power_demanded(:,k)*100;
+%v(:,k)=((ones(1,nodes)*power(:,k+1))*ones(nodes,1)-Power_demanded(:,k)-(ones(1,nodes)*power_aux(:,k+1))*ones(nodes,1))./Power_demanded(:,k)*100;
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Power_demanded(:,k+1)=Power_demanded(:,k);
 subplot(2,2,1)
-plot(transpose(Anita))
-%plot(transpose(power))
-%plot(transpose(Aux))
+plot(transpose(power))
 grid on
 subplot(2,2,2)
-%plot(transpose(Global_cost))
-plot(transpose(Eder))
-grid on
-subplot(2,2,3)
-%p=plot(graph(Edges(:,1),Edges(:,2)));
-%highlight(p,minspantree(graph(Edges(:,1),Edges(:,2))))
-%plot(transpose(Power_demanded))
 plot(transpose(Global_cost))
 grid on
+subplot(2,2,3)
+p=plot(graph(Edges(:,1),Edges(:,2)));
+highlight(p,minspantree(graph(Edges(:,1),Edges(:,2))))
+grid on
 subplot(2,2,4)
-plot(transpose(power))
-%plot(transpose(Power_demanded))
+plot(transpose(Power_demanded))
 grid on
 
-sum(power(:,30000))
-sum(power(:,60000))
-sum(power(:,k+1))
+(sum(power(:,20000))-Power_demanded(1,20000))/Power_demanded(1,20000)*100
+(sum(power(:,40000))-Power_demanded(1,40000))/Power_demanded(1,40000)*100
+(sum(power(:,k+1))-Power_demanded(1,k))/Power_demanded(1,k)*100
